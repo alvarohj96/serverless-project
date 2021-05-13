b8491785abd1ba1d0cf542eda31e41fa880a0875
EC2 Default User <ec2-user@ip-172-31-0-60.ec2.internal>
2021-05-13 21:26:27 +0000

    Añadido comentario de prueba

--- todos/get.py
+++ todos/get.py
@@ -5,6 +5,7 @@ from todos import decimalencoder
 import boto3
 dynamodb = boto3.resource('dynamodb')
 
+#Comentario de prueba
 
 def get(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
