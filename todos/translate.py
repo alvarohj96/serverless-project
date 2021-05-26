cbfecec4dc2a2aa98101ab9b19a22733584b44ce
alvarohj96 <alvarohj96sg@gmail.com>
2021-05-26 08:45:58 +0000

    Funcion translate

---
+++ todos/translate.py
@@ -0,0 +1,28 @@
+import os
+import json
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+def translate(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    
+    # fetch todo from the database
+    item_translate = table.get_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+    
+    
+    client = boto3.client(service_name='translate')
+    result = client.translate_text(Text=item_translate['Item']['text'], SourceLanguageCode="auto", TargetLanguageCode=event['pathParameters']['language'])  
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result.get('TranslatedText'))
+    }
+    
+    return response
